21484088f9f55a8115cd3170f302ef2f45470fa8
normalperson543 <148504965+normalperson543@users.noreply.github.com>
2026-08-30 02:09:54 -0700

    fix issue with wrong resolver attribution (because somehow that wasn't fixed before?)

--- src/lib/tools.ts
+++ src/lib/tools.ts
@@ -1,10 +1,14 @@
 import { getSlackUser } from "./data.js";
 
 export async function getResolver(str: string) {
-  if (str.indexOf("<@") === -1) {
+  const unescaped = str
+    .replace(/&lt;/g, "<")
+    .replace(/&gt;/g, ">")
+    .replace(/&amp;/g, "&");
+  if (unescaped.indexOf("<@") === -1) {
     return;
   }
-  const firstPart = str.substring(str.indexOf("<@") + 2);
+  const firstPart = unescaped.substring(unescaped.indexOf("<@") + 2);
   const userId = firstPart.substring(0, firstPart.indexOf(">"));
   return await getSlackUser(userId);
 }
